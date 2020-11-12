40a3b7ef0468c65dece60e3657a0b9e48781fbc5
Brijesh <brijesh.a@yudiz.in>
2020-11-12 17:55:15 +0530

    Jquery removed and ajax request added

--- dist/js/ajax.js
+++ dist/js/ajax.js
@@ -1,21 +1,24 @@
 function checkQrCode() {
-        var params = [], hash;
-        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-        for(var i = 0; i < hashes.length; i++)
-        {
-            hash = hashes[i].split('=');
-            params.push(hash[0]);
-            params[hash[0]] = hash[1];
-        }
-        console.log('hash', hashes);
-        console.log('params', params);
-        $.get('http://54.177.108.151/api/v1/qr', {qrcode: hash || 'NmqYvTA7ytTE20FHSUlOOGfpwTdIlD3oj0IuUI0lnr'}, (data, status) => {
-          if (data.status === 200) {
-            alert("Successfully run!!");
-          } else if (data.status === 403) {
-            window.location.replace('./verified.html')
-          } else if (data.status === 404) {
-            window.location.replace('./Not_Verified.html')
-          }
-        });
+  var params = [], hash;
+  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+  for (var i = 0; i < hashes.length; i++) {
+    hash = hashes[i].split('=');
+    params.push(hash[0]);
+    params[hash[0]] = hash[1];
+  }
+  const xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function (event) {
+    if (this.status && this.response) {
+      if (JSON.parse(this.response).status === 200) {
+        alert("Successfully run!!");
+      } else if (JSON.parse(this.response).status === 403) {
+        window.location.replace('../verified.html')
+      } else if (JSON.parse(this.response).status === 404) {
+        window.location.replace('../Not_Verified.html')
       }
+    }
+    
+  };
+  xhttp.open("GET", "http://54.177.108.151/api/v1/qr?qrcode=" + hash || 'NmqYvTA7ytTE20FHSUlOOGfpwTdIlD3oj0IuUI0lnr', true);
+  xhttp.send();
+}
