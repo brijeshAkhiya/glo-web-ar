3aa44daab810f67ef3b8daf12a17800f9ceb47ac
Brijesh <brijesh.a@yudiz.in>
2020-11-20 04:11:04 +0530

    Final test 8

--- dist/js/script1.js
+++ dist/js/script1.js
@@ -1,34 +1,35 @@
-const raycaster = new THREE.Raycaster();
-const mouse = new THREE.Vector2();
-
-function onMouseMove( event ) {
-
-	// calculate mouse position in normalized device coordinates
-	// (-1 to +1) for both components
-
-	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-
+const isAndroid = /android/i.test(navigator.userAgent);
+if (isAndroid) {
+	start();
+} else {
+	window.location.replace('../ar.html');
+}
+function start(result) {
+	result()
 }
 
-function render() {
 
-	// update the picking ray with the camera and mouse position
-	raycaster.setFromCamera( mouse, camera );
-
-	// calculate objects intersecting the picking ray
-	const intersects = raycaster.intersectObjects( scene.children );
-
-	for ( let i = 0; i < intersects.length; i ++ ) {
-
-		intersects[ i ].object.material.color.set( 0xff0000 );
-
-	}
-
-	renderer.render( scene, camera );
-
+function activateAr() {
+	navigator.xr.requestSession('immersive-ar')
+		.then((session) => {
+			if (session) {
+				document.getElementById('ar').click();
+			}
+		})
+		.catch((err) => {
+			document.getElementById('ar').innerHTML = "AR not supported";
+			document.getElementById('ar').disabled = true;
+			console.log(err);
+		})
 }
-
-window.addEventListener( 'mousemove', onMouseMove, false );
-
-window.requestAnimationFrame(render);
+function withoutPermission() {
+	document.querySelector('model-viewer').addEventListener('load', () => {
+		if (document.querySelector('button')) {
+			setInterval(() => {
+				if (!document.querySelector('button').click()) {
+					document.querySelector('button').click();
+				}
+			}, 2000)
+		}
+	});
+}
