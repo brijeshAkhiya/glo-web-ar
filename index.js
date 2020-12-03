f6685141f2a67688b38f936f675e13c201f7e68f
Brijesh <brijesh.a@yudiz.in>
2020-12-03 16:40:46 +0530

    New Update added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/dist'));
+
 app.listen(process.env.PORT || 3000, (err, result) => {
   if (err)  {
     console.log(err)
