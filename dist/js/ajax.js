e6905cba03ae8f2128174035a3e96a8c75d26514
Brijesh Akhiya <brijeshakhiya@gmail.com>
2020-12-29 12:16:58 +0530

    New Url Added

--- dist/js/ajax.js
+++ dist/js/ajax.js
@@ -3,17 +3,18 @@ const domain = 'https://glo-ar-web.herokuapp.com'
         const qrCode = window.location.href.split('/')[4];
         const xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function (event) {
-          console.log(this.response, qrCode);
           if (this.status && this.response) {
             if (JSON.parse(this.response).status === 200) {
               window.location.replace(domain + '/verified?url=' + JSON.parse(this.response).destination_url);
             } else if (JSON.parse(this.response).status === 403) {
-              alert(JSON.parse(this.response).message);
+              window.location.replace(domain + '/notverified');
             } else if (JSON.parse(this.response).status === 404) {
               window.location.replace(domain + '/notverified');
             }
           }
         };
-        xhttp.open("GET", "http://54.177.108.151/api/v1/qr?qrcode=" + qrCode, true);
+        xhttp.open("GET", "https://verify.qrcodesafe.com/api/v1/qr?qrcode=" + qrCode, true);
         xhttp.send();
       }
+
+      
