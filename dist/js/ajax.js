a6839a972059aef6ed187801af4a65415ff6dfab
Brijesh Akhiya <brijesh.a@yudiz.in>
2021-01-04 20:43:09 +0530

    New Build Added and Name Changed

--- dist/js/ajax.js
+++ dist/js/ajax.js
@@ -5,12 +5,12 @@ const domain = 'https://glo-ar-web.herokuapp.com'
         xhttp.onreadystatechange = function (event) {
           if (this.status && this.response) {
             if (JSON.parse(this.response).status === 200) {
-              window.location.replace(domain + '/verified');
+              window.location.replace(domain + '/Verified-QR-Code');
               localStorage.setItem('url', JSON.parse(this.response).destination_url);
             } else if (JSON.parse(this.response).status === 403) {
-              window.location.replace(domain + '/notverified');
+              window.location.replace(domain + '/In-Valid-QR-Code');
             } else if (JSON.parse(this.response).status === 404) {
-              window.location.replace(domain + '/notverified');
+              window.location.replace(domain + '/In-Valid-QR-Code');
             }
           }
         };
