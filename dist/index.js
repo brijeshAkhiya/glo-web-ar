f188030754c2328a37e82e032bea4fdb68a25127
Brijesh <brijesh.a@yudiz.in>
2021-02-11 03:20:28 +0530

    Android 50s working

--- dist/index.js
+++ dist/index.js
@@ -4,30 +4,30 @@ app.use('/scanqr', express.static(__dirname));
 app.use(express.static(__dirname));
 
 app.get('/scanqr/:qrcode', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
+    res.sendFile(__dirname + '/index.html')
 });
 
 app.get('/Verified-QR-Code', (req, res) => {
-  res.sendFile(__dirname + '/verified.html');
+    res.sendFile(__dirname + '/verified.html');
 });
 
-app.get('/Invalid-QR-Code', (req, res) =>{
-  res.sendFile(__dirname + '/notverified.html');
+app.get('/Invalid-QR-Code', (req, res) => {
+    res.sendFile(__dirname + '/notverified.html');
 });
-app.get('/test', (req, res) =>{
-  res.sendFile(__dirname + '/test.html');
+app.get('/test', (req, res) => {
+    res.sendFile(__dirname + '/test.html');
 });
-app.get('/not-verified-ar', (req, res) =>{
-  res.sendFile(__dirname + '/test2.html');
+app.get('/not-verified-ar', (req, res) => {
+    res.sendFile(__dirname + '/test2.html');
 });
-app.get('/verified-ar', (req, res) =>{
-  res.sendFile(__dirname + '/test1.html');
+app.get('/verified-ar', (req, res) => {
+    res.sendFile(__dirname + '/test1.html');
 });
 
 app.listen(process.env.PORT || 3000, (err, result) => {
-  if (err)  {
-    console.log(err)
-  } else {
-    console.log('connected');
-  }
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('connected');
+    }
 });
