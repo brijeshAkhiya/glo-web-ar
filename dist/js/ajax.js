0490845ab2499384ac11742f4e1adff796856c8f
brijeshAkhiya <brijeshakhiya@gmail.com>
2021-06-24 12:46:23 +0530

    Domain issue solved

--- dist/js/ajax.js
+++ dist/js/ajax.js
@@ -1,20 +1,16 @@
-// const domain = 'https://verify.gloextract.com'
-const domain = 'https://glo-ar-web.herokuapp.com';
 function checkQrCode() {
   const qrCode = window.location.href.split('/')[4];
   const xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function (event) {
     if (this.status && this.response) {
       if (JSON.parse(this.response).status === 200) {
-        window.location.replace(domain + '/Verified-QR-Code');
+        window.location.replace(window.location.origin + '/Verified-QR-Code');
         localStorage.setItem('url', JSON.parse(this.response).destination_url);
       } else if (JSON.parse(this.response).status === 403) {
-        // window.location.replace(domain + '/Invalid-QR-Code');
         window.location.replace('https://www.gloextract.com/not-verified');
       } else if (JSON.parse(this.response).status === 404) {
         // window.location.replace(domain + '/invalid-QR-Code');
         window.location.replace('https://www.gloextract.com/not-verified');
-        // window.location.replace('http://localhost:3000/verified-ar');
       }
     }
   };
